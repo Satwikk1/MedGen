f9a407f5e85890277945f1383970665737c0899a
Satwik Singh <satwiksingh@Satwiks-MacBook-Air.local>
2021-09-25 17:26:24 +0530

    styled login card

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,55 @@ import '../styles/main.scss';
 const Login = () => {
     return (
        <div className="auth-container">
-
+           <div className="about"></div>
+           <div className="card-container">
+                <div className="card-body">
+                    <form className="login">
+                        <h1 className="login-heading">Login</h1>
+                        <div className="email">
+                            <div className="login-logo"><span class="material-icons">account_circle</span></div>
+                            <input type="email" placeholder="Email"></input>
+                        </div>
+                        <div className="password">
+                            <div className="login-logo"><span class="material-icons">password</span></div>
+                            <input type="password" placeholder="Password"></input>
+                        </div>
+                        <div className="submit">
+                            <button onClick={
+                                // handle login logic
+                                ()=>{}
+                            }>Login</button>
+                        </div>
+                        <div className="divider">
+                            <div className="hr"></div>
+                            <h2>or</h2>
+                            <div className="hr"></div>
+                        </div>
+                        <div className="socialAuth">
+                            <div className="gmail">
+                                <button onClick={
+                                    // handle social auth logic
+                                    ()=>{}
+                                }>Gmail</button>
+                            </div>
+                            <div className="github">
+                                <button onClick={
+                                    // handle social auth logic
+                                    ()=>{}
+                                }>GitHub</button>
+                            </div>
+                        </div>
+                    </form>
+                    <div className="signup">
+                        <div className="heading">
+                            <h1 onClick={
+                                // trigger signup animation
+                                ()=>{}
+                            }>SignUp?</h1>
+                        </div>
+                    </div>
+                </div>
+           </div>
        </div> 
     )
 }
